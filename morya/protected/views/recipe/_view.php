f76cb40ecfbe32debdc16f248aa75ae4615b5ceb
mayureshn <mayuresh.naik001@gmail.com>
2012-08-26 00:37:14 -0700

    slug2 reciepe

--- morya/protected/views/recipe/_view.php
+++ morya/protected/views/recipe/_view.php
@@ -1,15 +1,6 @@
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('slug')); ?>:</b>
-	<?php echo CHtml::encode($data->slug); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
-	<?php echo CHtml::encode($data->title); ?>
+	<h2 style="text-align:center;"><?php echo CHtml::encode($data->title); ?></h2>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('cooking_time')); ?>:</b>
@@ -22,10 +13,6 @@
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('method')); ?>:</b>
 	<?php echo CHtml::encode($data->method); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('created')); ?>:</b>
-	<?php echo CHtml::encode($data->created); ?>
 	<br />
 
 	<?php /*
