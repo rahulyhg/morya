6376997d92753a7cbd6f10334640d7621e133ee3
mayureshn <mayuresh.naik001@gmail.com>
2012-09-05 18:10:52 +0530

    css changes

--- morya/protected/views/vedic/vedic.php
+++ morya/protected/views/vedic/vedic.php
@@ -22,16 +22,21 @@ $this->menu=array(
 	array('label'=>'Uttar Pooja', 'url'=>array('vedic','vedicType'=>VedicType::Pooja)),
 );
 ?>
-<div class="tab"><?php echo $heading;?></div>
-<div style="float:right;font:14px;"><?php echo CHtml::link($addhead,array('addvedic','vedicType'=>$vedicType));?></div>
-<?php foreach($elementsList as $vedic){ ?>
-<div><span><h3><?php echo $vedic->title;?><h3></span>
-<?php if($vedicType == VedicType::Aarti || VedicType::Mantra){ ?>
-<span><?php echo CHtml::link($addhead,array('addvedic','vedicType'=>$vedicType));?><span></div>
-<?php } ?>
-<div><?php echo $vedic->name_of_god;?></div>
-<div><?php echo $vedic->text;?></div>
-<?php } ?>
+<div class="mid-region">
+    <div class="tab"><?php echo $heading;?></div>
+    <div style="float:right;font:14px;"><?php echo CHtml::link($addhead,array('addvedic','vedicType'=>$vedicType));?></div>
+    <div class="cont-disp"></div>
+        <?php foreach($elementsList as $vedic){ ?>
+        <div><span><h3><?php echo $vedic->title;?><h3></span>
+        <?php if($vedicType == VedicType::Aarti || VedicType::Mantra){ ?>
+        <span><?php echo CHtml::link($addhead,array('addvedic','vedicType'=>$vedicType));?><span>
+        </div>
+        <?php } ?>
+        <div><?php echo $vedic->name_of_god;?></div>
+        <div><?php echo $vedic->text;?></div>
+        <?php } ?>
+    </div>
+</div>
 
 <div style="float:right width:30%">
 <h3>Find more <?php echo $heading ?> here<h3>
