de2999df041136c044ea78de79a5a9f941f4a2eb
mayureshn <mayuresh.naik001@gmail.com>
2013-07-23 10:45:49 +0530

    22july changes

---
+++ devaganesha/wordpress/wp-content/themes/morya/blog.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Template Name: morya blog page
+*/
+get_header(); 
+?>
+
+<div class="row-fluid mt10" >
+<?php
+get_sidebar('left');
+?>
+
+
+<div class="span6">
+	<div class="title-bar">Blogs</div>
+<?php
+query_posts('showposts=10');
+if(have_posts()) :
+	while(have_posts()) : the_post();
+?>	
+	<div class="blogs">
+		<div>
+			<img src="<?php echo get_template_directory_uri(); ?>/img/gan1.png" class="fl mr10" />
+					<p class="fnt24 mt5"><a href="<?php echo the_permalink() ; ?>"><b><?php echo the_title() ; ?></b></a></p>
+					<p>Posted on: <?php the_date(); ?> | author: <?php the_author();?></p>
+					
+		</div>
+		<div class="clear"></div>
+		<div class="blog-content">
+			<?php the_excerpt(); ?>
+		</div>
+		<div><span>posted in : <?php the_category(','); ?> </span> | <span> <a href="<?php echo the_permalink() ; ?>">Leave reply </a></span></div>
+	
+	</div>
+	
+		<?php
+
+	endwhile ;
+endif;
+?>
+</div>
+		<?php
+get_sidebar('right');
+?>
+</div>
+
+<?php get_footer(); ?>
---
+++ devaganesha/wordpress/wp-content/themes/morya/single.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Template Name: morya single page
+*/
+get_header(); 
+?>
+
+<div class="row-fluid mt10" >
+<?php
+get_sidebar('left');
+?>
+
+
+<div class="span6">
+	<div class="title-bar">Blogs</div>
+<?php
+if(have_posts()) :
+	while(have_posts()) : the_post();
+?>	
+	<div class="blogs">
+		<div>
+			<img src="<?php echo get_template_directory_uri(); ?>/img/gan1.png" class="fl mr10" />
+					<p class="fnt24 mt5"><a href="<?php echo the_permalink() ; ?>"><b><?php echo the_title() ; ?></b></a></p>
+					<p>Posted on: <?php the_time(); ?> | author: <?php the_author();?></p>
+		</div>
+		<div class="blog-content">
+			<?php the_excerpt(); ?>
+		</div>
+		<div><span>posted in : news category </span></div>
+		<?php comments_template( '', true ); ?>
+	</div>
+	
+	
+		<?php
+
+	endwhile ;
+endif;
+?>
+</div>
+		<?php
+get_sidebar('right');
+?>
+</div>
+
+<?php get_footer(); ?>
