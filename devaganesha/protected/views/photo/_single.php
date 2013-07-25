ae3aeb224d254b0f39a1413130f5dec1b353b8cb
mayureshn <mayuresh.naik001@gmail.com>
2013-07-25 06:47:41 +0530

    dynamic photo gallery

--- devaganesha/protected/views/photo/_single.php
+++ devaganesha/protected/views/photo/_single.php
@@ -1,17 +1,17 @@
-<li class="tj_row_1">
+<!--<li class="tj_row_1">
     <div class="photo_thumb">
-        <h3><?php $photo->caption ;?></h3>
+        <h3><?php //$photo->caption ;?></h3>
         <?php
-        $img = PhotoType::$folderName[PhotoType::Thumb] . $photo->file_name;
-        list($width, $height, $type, $attr) = getimagesize($img);
+       // $img = PhotoType::$folderName[PhotoType::Thumb] . $photo->file_name;
+        //list($width, $height, $type, $attr) = getimagesize($img);
         ?>
-        <a href="<?php echo Yii::app()->createUrl('photo/view',array('slug'=>$photo->slug)) ?>">
-            <img src="<?php echo PhotoType::$relativeFolderName[PhotoType::Thumb] . $photo->file_name ;?>" style="width:<?php echo PhotoType::$dimension[PhotoType::Thumb]['width'] ?>px;height:<?php echo  PhotoType::$dimension[PhotoType::Thumb]['height'] ?>px;" />
+        <a href="<?php // echo Yii::app()->createUrl('photo/view',array('slug'=>$photo->slug)) ?>">
+            <img src="<?php //echo PhotoType::$relativeFolderName[PhotoType::Thumb] . $photo->file_name ;?>" style="width:<?php //echo PhotoType::$dimension[PhotoType::Thumb]['width']; ?>px;height:<?php //echo  PhotoType::$dimension[PhotoType::Thumb]['height'] ?>px;" />
         </a>
         <div class="white_mask_wrapper">
             <div class="white_mask">
                 <span class="darshan"><p>789</p></span>
-                <span class="modak"><p><?php echo count($photo->node->modaks); ?></p></span>
+                <span class="modak"><p><?php //echo count($photo->node->modaks); ?></p></span>
                 <div class="add_collection">
 				<a class="addthis_button_compact"></a>
 				</div>
@@ -19,4 +19,19 @@
             </div>
         </div>
     </div>
-</li>
+</li> -->
+       <?php
+        $img = PhotoType::$folderName[PhotoType::Screen] . $photo->file_name;
+        list($width, $height, $type, $attr) = getimagesize($img);
+        ?>
+
+		<div class="pin">
+			<a href="<?php echo Yii::app()->createUrl('photo/view',array('slug'=>$photo->slug)) ?>"><img src="<?php echo PhotoType::$relativeFolderName[PhotoType::Screen] . $photo->file_name ;?>" alt=""></a>
+			<p class="description"><?php echo $photo->original_name; ?></p>
+			<div class="convo">
+      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
+		        <p>
+		          <a href="">kedar shinde</a> via <a href="">amolwadi</a>
+		        </p>
+       		</div>
+		</div>
--- devaganesha/protected/views/photo/index.php
+++ devaganesha/protected/views/photo/index.php
@@ -24,142 +24,17 @@ $this->breadcrumbs=array(
             )
         )); ?>
 
-<!--<section id="photo_container" style="position:relative;">
-    <div id="photo_wrapper" class="photo_wrapper" style="position:relative;width:100%;height:100%;">
-        <ul id="photo_gallery" class="photo_gallery">
-    <?php
-       /*  foreach($elementsList as $photo){
-            $this->renderPartial('//photo/_single',array('photo'=>$photo));
-        } */
-    ?>
-        </ul>
-    </div>
-</section> -->
-
     <div class="row-fluid" >
       <div id="container">
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/1.jpg" alt="">
-			<p class="description">Game Icon</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		          <a href="">kedar shinde</a> via <a href="">amolwadi</a>
-		        </p>
-       		</div>
-		</div>
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/2.jpg" alt="">
-			<p class="description">DJ iPad app</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		          <a href="">bharat jadhav</a> via <a href="">khetwadi</a>
-		        </p>
-       		</div>
-		</div>
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/3.jpg" alt="">
-			<p class="description">Food iPhone app</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		          <a href="">ankush chudhari</a> via <a href="">thane</a>
-		        </p>
-       		</div>
-		</div>
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/4.jpg" alt="">
-			<p class="description">.net Magazine</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		           <a href="">swapnil joshi</a> via <a href="">kurla</a>
-		        </p>
-       		</div>
-		</div>
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/5.jpg" alt="">
-			<p class="description">Starbucks international landing page</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		          <a href="">Aaron Lumsden</a> via <a href="">Aaronlumsden.com</a>
-       		</div>
-		</div>
-
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/6.jpg" alt="">
-			<p class="description">Game Icon</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		          <a href="">Aaron Lumsden</a> via <a href="">Aaronlumsden.com</a>
-		        </p>
-       		</div>
-		</div>
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/7.jpg" alt="">
-			<p class="description">DJ iPad app</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		          <a href="">Aaron Lumsden</a> via <a href="">Aaronlumsden.com</a>
-		        </p>
-       		</div>
-		</div>
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/8.jpg" alt="">
-			<p class="description">Food iPhone app</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		          <a href="">Aaron Lumsden</a> via <a href="">Aaronlumsden.com</a>
-		        </p>
-       		</div>
-		</div>
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/9.jpg" alt="">
-			<p class="description">.net Magazine</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		           <a href="">Aaron Lumsden</a> via <a href="">Aaronlumsden.com</a>
-		        </p>
-       		</div>
-		</div>
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/10.jpg" alt="">
-			<p class="description">Starbucks international landing page</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		          <a href="">Aaron Lumsden</a> via <a href="">Aaronlumsden.com</a>
-       		</div>
-		</div>
-
-
-		<div class="pin">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/5.jpg" alt="">
-			<p class="description">Game Icon</p>
-			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
-		        <p>
-		          <a href="">Aaron Lumsden</a> via <a href="">Aaronlumsden.com</a>
-		        </p>
-       		</div>
-		</div>
-      </div> 
+    <?php
+         foreach($elementsList as $photo){
+            $this->renderPartial('//photo/_single',array('photo'=>$photo));
+        }
+    ?>
+     </div> 
 	</div>
+
+ 
 
 <?php $this->beginClip('js-page-end'); ?>
 		<script type="text/javascript">
