ae71c7c657523bd59a90dcbc96b1516524369855
mayureshn <mayuresh.naik001@gmail.com>
2013-07-28 20:07:30 +0530

    photo single page 28july

--- devaganesha/protected/views/comment/create.php
+++ devaganesha/protected/views/comment/create.php
@@ -9,13 +9,13 @@
     <?php
     if(Yii::app()->user->isGuest):
     ?>
-    <div class="row">
+    <div>
         <?php echo $form->labelEx(User::model(),'name'); ?>
         <?php echo $form->textField(User::model(),'name',array('size'=>27,'maxlength'=>255)); ?>
         <?php echo $form->error(User::model(),'name'); ?>
     </div>
 
-        <div class="row">
+        <div>
             <?php echo $form->labelEx(User::model(),'email'); ?>
             <?php echo $form->textField(User::model(),'email',array('size'=>27,'maxlength'=>255)); ?>
             <?php echo $form->error(User::model(),'email'); ?>
@@ -25,12 +25,12 @@
     endif;
     ?>
 	<?php echo $form->hiddenField($comment,'node_id',array('value'=>$comment->node_id)); ?>
-    <div class="row">
+    <div>
         <?php echo $form->textArea($comment,'comment',array('size'=>80,'maxlength'=>255)); ?>
     </div>
 
-    <div class="row">
-        <?php echo CHtml::submitButton('Comment',array('class'=>'button_1')); ?>
+    <div>
+        <?php echo CHtml::submitButton('Comment',array('class'=>'btn')); ?>
     </div>
 
     <?php $this->endWidget(); ?>
--- devaganesha/protected/views/photo/view.php
+++ devaganesha/protected/views/photo/view.php
@@ -1,4 +1,26 @@
-<div style="float:left;">
+
+        <?php $this->widget('ext.EAjaxUpload.EAjaxUpload',
+        array(
+            'id'=>'uploadFile',
+            'config'=>array(
+                'action'=>Yii::app()->createUrl('photo/postUpload',array('type'=>PhotoUploadCategory::Normal)),
+                'allowedExtensions'=>array("jpg","jpeg","gif"),//array("jpg","jpeg","gif","exe","mov" and etc...
+                'sizeLimit'=>10*1024*1024,// maximum file size in bytes
+                'minSizeLimit'=>10,// minimum file size in bytes
+                'onComplete'=>"js:function(id,filename,response){}",
+                'uploadButtonText'=>'Drop Your Ganesha`s Photoes Here to Upload. ',
+                'messages'=>array(
+                    'typeError'=>"{file} has invalid extension. Only {extensions} are allowed.",
+                    'sizeError'=>"{file} is too large, maximum file size is {sizeLimit}.",
+                    'minSizeError'=>"{file} is too small, minimum file size is {minSizeLimit}.",
+                    'emptyError'=>"{file} is empty, please select files again without it.",
+                    'onLeave'=>"The files are being uploaded, if you leave now the upload will be cancelled."
+                ),
+                'showMessage'=>"js:function(message){ alert(message); }"
+            )
+        )); ?>
+<div class="row-fluid">
+<div class="span6">
     <div id="modak-rating">
 	<?php
 		$this->widget('CStarRating',array(
@@ -30,10 +52,9 @@
 	));?>
 	</div>
 	<img style="width:<?php echo PhotoType::$dimension[PhotoType::Screen]['width'] ?>;" src="<?php echo PhotoType::$relativeFolderName[PhotoType::Screen].$photo->file_name ?>" />
-    <div class="caption"><?php echo $photo->caption ?></div>
-    <p style="font-size:12px "><em>By: </em><a class="photo_uploader_name"><?php echo $photo->node->creator->name ?></a></p>
+    <div class="caption"><?php echo $photo->original_name; ?></div>
+    <p style="font-size:12px "><em>Posted By: </em><a class="photo_uploader_name"><?php echo $photo->node->creator->name ?></a></p>
 	
-</div>
 
 
 	<?php $this->beginClip('js-page-end'); ?>
@@ -42,11 +63,16 @@
 			</script>
 	<?php $this->endClip(); ?>
 
-<div id="comments">
-    <?php
-    foreach($photo->node->comments as $comment){
-        $this->renderPartial('//comment/view',array('comment'=>$comment));
-    }
-    ?>
-<?php $this->renderPartial('//comment/create',array('comment'=>$newComment)); ?>
+</div>
+	<div class="span6">
+		<div id="comments">
+			<?php
+			foreach($photo->node->comments as $comment){
+				$this->renderPartial('//comment/view',array('comment'=>$comment));
+			}
+			?>
+		<?php $this->renderPartial('//comment/create',array('comment'=>$newComment)); ?>
+		</div>
+	</div>
+
 </div>
