710d6391464d00f2fb82fb83a75222c168128d31
mayureshn <mayuresh.naik001@gmail.com>
2013-07-30 21:44:20 +0530

    30july changes

--- devaganesha/protected/views/photo/_single.php
+++ devaganesha/protected/views/photo/_single.php
@@ -26,12 +26,23 @@
         ?>
 
 		<div class="pin">
+		  <div class="white_mask_wrapper">
+            <div class="white_mask">
+                <span class="darshan"><p>789</p></span>
+                <span class="modak"><p><?php //echo count($photo->node->modaks); ?></p></span>
+                <div class="add_collection">
+				<a class="addthis_button_compact"></a>
+				</div>
+				<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-517d3bd171dee465"></script>
+            </div>
+        </div>
 			<a href="<?php echo Yii::app()->createUrl('photo/view',array('slug'=>$photo->slug)) ?>"><img src="<?php echo PhotoType::$relativeFolderName[PhotoType::Screen] . $photo->file_name ;?>" alt=""></a>
-			<p class="description"><?php echo $photo->original_name; ?></p>
+			
+			<div class="description"><?php echo $photo->original_name; ?></div>
 			<div class="convo">
-      			<img src="<?php echo get_template_directory_uri(); ?>/images/avatar.jpg">
+      			<img src="<?php echo get_template_directory_uri(); ?>/img/prof.gif" alt="pp" />
 		        <p>
-		          <a href="">kedar shinde</a> via <a href="">amolwadi</a>
+		          <a href=""><?php echo $photo->node->creator->name; ?></a> via <a href="">amolwadi</a>
 		        </p>
        		</div>
 		</div>
