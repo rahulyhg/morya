f519e714c60dd7a1ec08062468312929770f45ef
mayureshn <mayuresh.naik001@gmail.com>
2013-08-10 17:38:05 +0530

    add this share

--- devaganesha/protected/views/photo/view.php
+++ devaganesha/protected/views/photo/view.php
@@ -75,10 +75,10 @@
 	</div> -->
 	<!-- AddThis Button BEGIN -->
 	<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-	<a class="addthis_button_preferred_2"></a>
-	<a class="addthis_button_preferred_3"></a>
-	<a class="addthis_button_preferred_4"></a>
-	<a class="addthis_button_preferred_10"></a>
+		<a class="addthis_button_facebook"></a>
+	<a class="addthis_button_twitter"></a>
+	<a class="addthis_button_pinterest_share"></a>
+	<a class="addthis_button_email"></a>
 	<a class="addthis_button_compact"></a>
 	<a class="addthis_counter addthis_bubble_style"></a>
 	</div>
