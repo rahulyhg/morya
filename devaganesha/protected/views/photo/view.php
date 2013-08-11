838e26f0a1656184ab65dcdf3e7db949066b780b
mayureshn <mayuresh.naik001@gmail.com>
2013-08-12 00:37:05 +0530

    home page and social on single page

--- devaganesha/protected/views/photo/view.php
+++ devaganesha/protected/views/photo/view.php
@@ -64,7 +64,7 @@
 	</div>
 	<!--<div class="visit-vw">View : <?php echo $novisit;?></div> -->
 	<div class="visit-vw"><span class="btn">View</span><span class="btn"><?php echo $novisit;?></span></div>
-	<div class="visit-vw">
+	<!--<div class="visit-vw">
 	<span class="share32 fb"></span>
 	<span class="share32 tw"></span>
 	<span class="share32 pi"></span>
@@ -72,7 +72,19 @@
 	<span class="share32 fl"></span>
 	<span class="share32 st"></span>
 	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-517d3bd171dee465"></script>
+	</div> -->
+	<!-- AddThis Button BEGIN -->
+	<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+	<a class="addthis_button_preferred_2"></a>
+	<a class="addthis_button_preferred_3"></a>
+	<a class="addthis_button_preferred_4"></a>
+	<a class="addthis_button_preferred_10"></a>
+	<a class="addthis_button_compact"></a>
+	<a class="addthis_counter addthis_bubble_style"></a>
 	</div>
+	<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-517d3bd171dee465"></script>
+<!-- AddThis Button END -->
 </div>
 	<div class="single-photo">
 		<img style="width:<?php echo PhotoType::$dimension[PhotoType::Screen]['width'] ?>;" src="<?php echo PhotoType::$relativeFolderName[PhotoType::Screen].$photo->file_name ?>" class="large-img"/>
