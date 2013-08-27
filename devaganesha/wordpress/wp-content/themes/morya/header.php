1a6f306406be094d2a95d0b9032bc8cbd5996333
Swapnil Sunil Gondkar <swapnil.gondkar@gmail.com>
2013-08-28 00:06:53 +0530

    added images and recent posts sidebar functionality

--- devaganesha/wordpress/wp-content/themes/morya/header.php
+++ devaganesha/wordpress/wp-content/themes/morya/header.php
@@ -2,14 +2,8 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
     <title><?php echo Yii::app()->controller->pageTitle != '' ? Yii::app()->controller->pageTitle :  Yii::app()->name . wp_title('-',false,'left') ; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
     <!-- Le styles -->
     <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css" rel="stylesheet">
