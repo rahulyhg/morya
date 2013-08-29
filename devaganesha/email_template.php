acbd2dd17afd5a27922d855cada6c00fcc20191a
mayureshn <mayuresh.naik001@gmail.com>
2013-08-29 19:54:48 +0530

    email template

---
+++ devaganesha/email_template.php
@@ -0,0 +1,77 @@
+<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
+    <tr>
+        <td align="center" valign="top">
+            <table border="0" cellpadding="20" cellspacing="0" width="600" id="emailContainer">
+                <tr>
+                    <td align="center" valign="top">
+                        <table border="0" cellpadding="20" cellspacing="0" width="100%" id="emailHeader">
+                            <tr>
+                                <td align="center" valign="top">
+                                   <img src="http://www.devaganesha.com/wordpress/wp-content/themes/morya/img/ganesha-logo.png" />
+                                </td>
+                            </tr>
+							<tr>
+                                <td valign="top">
+                                   Hello there,
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" valign="top">
+                        <table border="0" cellpadding="20" cellspacing="0" width="100%" id="emailBody">
+                            <tr>
+                                <td valign="top">
+                                    Your friend has invited you on <a href="http://www.devaganesha.com" target="_blank">devaganesha.com</a><br/>
+									Visit the site and get online darshan of ganesha's from all over india. 
+                                </td>
+                            </tr>
+                        </table>
+						
+						<table border="0" cellpadding="0" cellspacing="0" style="background-color:#0088cc; border:1px solid #353535; border-radius:5px;">
+    <tr>
+        <td align="center" valign="middle" style="color:#FFFFFF; font-family:Helvetica, Arial, sans-serif; font-size:16px; font-weight:bold; letter-spacing:-.5px; line-height:150%; padding-top:15px; padding-right:30px; padding-bottom:15px; padding-left:30px;">
+            <a href="http://www.devaganesha.com/" target="_blank" style="color:#FFFFFF; text-decoration:none;">Visit site</a>
+        <td>
+    </tr>
+</table>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" valign="top">
+                        <table border="0" cellpadding="20" cellspacing="0" width="100%" id="emailFooter">
+                            <tr>
+                                <td valign="top">
+                                    Thanks<br/>
+									Devaganesha.com
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+<style type="text/css">
+    .standardCSS{
+        color:#505050;
+        font-size:15px;
+    }
+
+    @media only screen and (max-width:480px){
+        .mediaQueryCSS{
+            color:#CCCCCC !important;
+            font-size:20px !important;
+        }
+    }
+	
+	    .bodyContent{
+        color:#505050;
+        font-family:Helvetica, Arial, sans-serif;
+        font-size:14px;
+        line-height:125%;
+    }
+</style>
